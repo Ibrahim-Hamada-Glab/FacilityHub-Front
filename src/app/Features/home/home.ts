f29a66322c3d5e86884ac5f793bb13de4ba14e2f
ibrahim <glabhamada@gmail.com>
2026-03-04 05:43:38 +0200

    feat: implement sidebar toggle functionality and enhance styles for better responsiveness

--- src/app/Features/home/home.ts
+++ src/app/Features/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { Dashboard } from "../dashboard/dashboard";
 import { Header } from "../header/header";
@@ -9,4 +9,36 @@ import { Header } from "../header/header";
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+  siderbarCollapsed = signal(false);
+
+  collapseSidebar(collapsed: boolean) {
+   
+    this.siderbarCollapsed.set(collapsed);
+
+
+    // const h1SidebarElements = document.querySelectorAll('app-sidebar span');
+    // const h1sidebarElement = document.querySelector('app-sidebar header h1');
+    // const appsidebarElement = document.querySelector('app-sidebar');
+    // console.log(h1SidebarElements);
+    //    if (collapsed) {
+    //     if (appsidebarElement) {
+    //       (appsidebarElement as HTMLElement).classList.remove('flex-1');
+    //     }
+    //     h1SidebarElements.forEach(el => (el as HTMLElement).classList.add('collapse'));
+    //     if (h1sidebarElement) {
+    //       (h1sidebarElement as HTMLElement).classList.add('collapse');
+    //     }
+    //     } else {
+    //     h1SidebarElements.forEach(el => (el as HTMLElement).classList.remove('collapse'));
+    //     if (h1sidebarElement) {
+    //       (h1sidebarElement as HTMLElement).classList.remove('collapse');
+    //     }
+    //      if (appsidebarElement) {
+    //       (appsidebarElement as HTMLElement).classList.add('flex-1');
+    //     }
+    //   }
+     
+  }
+}
--- src/app/Features/sidebar/sidebar.ts
+++ src/app/Features/sidebar/sidebar.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, input } from '@angular/core';
 import { RouterLink } from "@angular/router";
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
+  host:{'class.collapsed':'collapsed'}
 })
-export class Sidebar {}
+export class Sidebar {
+  collapsed = input(false);
+
+    @HostBinding('class.collapsed')
+  get isCollapsed() {
+    return this.collapsed();
+  }
+}
